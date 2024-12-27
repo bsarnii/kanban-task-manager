6cf2be5277519c03f2a832bdb5a0eaa95d342027
bsarnii <scharnoui@gmail.com>
2024-12-27 17:32:05 +0100

    chg: angular v17 update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -33,7 +31,6 @@ import { BoardModalFrameComponent } from './shared/board-modal-frame/board-modal
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     ReactiveFormsModule
   ],
   providers: [],
