cefa49b1f1107ad989ffcfa89815d4cc19a701b5
bsarnii <scharnoui@gmail.com>
2025-02-09 20:50:44 +0100

    chg: started to implement a general modal component

---
+++ src/app/shared/ui/modal/modal.component.ts
@@ -0,0 +1,11 @@
+import { Component, output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent {
+  backdropClick = output();
+}
