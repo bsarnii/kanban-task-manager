584854fa8e7389e16f50664ec32e9fd6f5b8c79b
bsarnii <scharnoui@gmail.com>
2025-04-16 21:39:45 +0200

    chg: improved auth error handling

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Router } from "@angular/router";
+import { catchError, EMPTY } from "rxjs";
 import { UsersStore } from "src/app/users/+store/users.store";
 import { environment } from "src/environments/environment";
 
@@ -12,14 +13,26 @@ export class AuthService {
     router = inject(Router);
 
     logIn(email: string, password: string, callback: VoidFunction) {
-        return this.http.post<{access_token: string}>(`${this.apiUrl}/auth/login`, {email, password}).subscribe((res => {
+        return this.http.post<{access_token: string}>(`${this.apiUrl}/auth/login`, {email, password}).pipe(
+            catchError((err:HttpErrorResponse) => {
+                //TODO: handle error properly
+                window.alert(err.error.message);
+                return EMPTY;
+            })
+        ).subscribe((res => {
             this.setAuthToken(res.access_token);
             callback();
         }));
     }
 
     signUp(signUpInputDTO: {name: string, email: string, password: string}, callback: VoidFunction) {
-        return this.http.post<unknown>(`${this.apiUrl}/users/signup`, signUpInputDTO).subscribe((() => {
+        return this.http.post<unknown>(`${this.apiUrl}/users/signup`, signUpInputDTO).pipe(
+            catchError((err:HttpErrorResponse) => {
+                //TODO: handle error properly
+                window.alert(err.error.message);
+                return EMPTY;
+            })
+        ).subscribe((() => {
             callback();
         }));
     }
