740c19b5763b83e60a1790c0aeb6480f76ef56a8
Arnold Schiopu <scharnoui@gmail.com>
2026-04-01 22:09:20 +0200

    chg: extracted primeNg theme preset config to a separate file

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,8 @@ import { unauthorizedInterceptor } from './app/core/interceptors/unauthorized.in
 
 //PrimeNG
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
-import { definePreset } from '@primeng/themes';
 import { MessageService } from 'primeng/api';
+import { themePreset } from './theme-preset';
 
 
 
@@ -34,48 +33,7 @@ bootstrapApplication(AppComponent, {
         ,),
       provideRouter(appRoutes, withComponentInputBinding()),
       provideAnimationsAsync(),
-      providePrimeNG({
-          theme: {
-            preset: definePreset(Aura, {
-              semantic: {
-                primary: {
-                    50: '{indigo.50}',
-                    100: '{indigo.100}',
-                    200: '{indigo.200}',
-                    300: '{indigo.300}',
-                    400: '{indigo.400}',
-                    500: '{indigo.500}',
-                    600: '{indigo.600}',
-                    700: '{indigo.700}',
-                    800: '{indigo.800}',
-                    900: '{indigo.900}',
-                    950: '{indigo.950}'
-                },
-                colorScheme: {
-                  dark: {
-                    surface: {
-                      50: '{zinc.50}',
-                      100: '{zinc.100}',
-                      200: '{zinc.200}',
-                      300: '{zinc.300}',
-                      400: '{zinc.400}',
-                      500: '{zinc.500}',
-                      600: '{zinc.600}',
-                      700: '{zinc.700}',
-                      800: '{zinc.800}',
-                      900: '#2B2C37',
-                      950: '#20212C'
-                    }
-                  }
-                }
-              }
-            }),
-            options: {
-              darkModeSelector: '.my-app-dark',
-              
-            } 
-          }
-      }),
+      providePrimeNG(themePreset),
       MessageService
     ],
   }).catch((err) => console.error(err));
