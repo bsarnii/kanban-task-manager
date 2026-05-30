04955bc31535536efbce504dd29c3d88b4085a7d
Arnold Schiopu <scharnoui@gmail.com>
2026-05-30 17:18:18 +0200

    chg: extended home page with features section

--- src/app/home/feature/home-page/home-page.component.ts
+++ src/app/home/feature/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ButtonDirective, ButtonLabel } from "primeng/button";
+import { CardModule } from 'primeng/card';
 import { RouterLink } from '@angular/router';
 import { ColorThemeService } from 'app/core/services/color-theme.service';
 import { PublicFooterComponent } from "app/shared/ui/public-footer/public-footer.component";
@@ -7,7 +8,7 @@ import { PublicHeaderComponent } from "app/shared/ui/public-header/public-header
 
 @Component({
   selector: 'app-home-page',
-  imports: [ButtonDirective, RouterLink, ButtonLabel, PublicFooterComponent, PublicHeaderComponent],
+  imports: [ButtonDirective, RouterLink, ButtonLabel, CardModule, PublicFooterComponent, PublicHeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
