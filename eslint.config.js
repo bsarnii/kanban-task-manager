f45ff92c7e29d421ba08553f08d42403cbe676f8
Arnold Schiopu <scharnoui@gmail.com>
2026-06-05 06:27:42 +0200

    add: prettier

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig([
       tseslint.configs.recommended,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      'prettier'
     ],
     processor: angular.processInlineTemplates,
     rules: {
@@ -38,6 +39,7 @@ module.exports = defineConfig([
     extends: [
       angular.configs.templateRecommended,
       angular.configs.templateAccessibility,
+      'prettier'
     ],
     rules: {},
   }
